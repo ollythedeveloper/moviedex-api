591f9a05144efa9487a25476bace53ad9c80444a
Olivia Lewis <ollybaby12@gmail.com>
2021-03-11 01:48:09 -0500

    add query param func to get request

--- server.js
+++ server.js
@@ -23,7 +23,30 @@ app.use(function validateBearerToken(req, res, next) {
 })
 
 function handleGetMovies(req, res) {
-    res.send('Hello Movies!')
+    let response = MOVIEDEX;
+
+    if(req.query.genre) {
+        response = response.filter(movie =>
+            movie
+                .genre
+                .toLowerCase()
+                .includes(req.query.genre.toLowerCase()));
+    }
+
+    if(req.query.country) {
+        response = response.filter(movie =>
+            movie
+                .country
+                .toLowerCase()
+                .includes(req.query.country.toLowerCase()));
+    }
+
+    if(req.query.avg_vote) {
+        response = response.filter(movie => 
+        Number(movie.avg_vote) >= Number(req.query.avg_vote));
+    }
+
+    res.json(response)
 }
 
 app.get('/movie', handleGetMovies)
