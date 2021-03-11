7471944403f03978f9c231215756d13149479d54
Olivia Lewis <ollybaby12@gmail.com>
2021-03-11 02:18:43 -0500

    secured for public

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const MOVIEDEX = require('./moviedex.json')
-
-console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
