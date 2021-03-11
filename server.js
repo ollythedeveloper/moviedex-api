69f4ad2b96c85e120e990516895740ed19e865ad
Olivia Lewis <ollybaby12@gmail.com>
2021-03-11 01:23:16 -0500

    add mw func and  moviedex data store

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const MOVIEDEX = require('./moviedex.json')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-    res.send('Hello Ollys world!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    console.log('validate bearer token middleware')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+    //move to the next middleware
+    next()
 })
+
+function handleGetMovies(req, res) {
+    res.send('Hello Movies!')
+}
+
+app.get('/movie', handleGetMovies)
 
 const PORT = 8000
 
