42fd04c9b47d4e241fa4bb067e9a08fe43d9f17b
Olivia Lewis <ollybaby12@gmail.com>
2021-03-11 13:54:05 -0500

    implement error message middleware

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
 
-    console.log('validate bearer token middleware')
-
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request'})
     }
@@ -53,6 +51,17 @@ function handleGetMovies(req, res) {
 }
 
 app.get('/movie', handleGetMovies)
+
+//4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
 
 const PORT = process.env.PORT || 8000
 
